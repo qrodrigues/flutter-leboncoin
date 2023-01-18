d920e74586777d7bc187451461e40e92181a93ff
leo9224 <leofetter92@gmail.com>
2023-01-18 10:18:45 +0100

    refactor: add loading message in l10n

--- lib/show_product/favorites_page.dart
+++ lib/show_product/favorites_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:leboncoin/firebase/product_repository.dart';
 import 'package:leboncoin/show_product/widgets/product_card.dart';
 import 'package:leboncoin/show_product/widgets/product_list.dart';
 
@@ -11,25 +12,28 @@ class FavoritesPage extends StatefulWidget {
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
-  /*
-  final List<ProductCard> products = [
-    const ProductCard('Trottinette électrique Xiaomi', 200, "test"),
-    const ProductCard('Table en bois', 140, "test"),
-  ];
-
-   */
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.favorites),
-      ),
-      backgroundColor: Colors.greenAccent.withOpacity(0.8),
-      body: CustomScrollView(
-        primary: false,
-        slivers: <Widget>[ProductList([])],
-      ),
+    return FutureBuilder<List<ProductCard>>(
+      future: ProductRepository().getAllProducts(),
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(AppLocalizations.of(context)!.adverts),
+            ),
+            backgroundColor: Colors.greenAccent.withOpacity(0.8),
+            body: CustomScrollView(
+              primary: false,
+              slivers: <Widget>[
+                ProductList(snapshot.data! as List<ProductCard>)
+              ],
+            ),
+          );
+        }
+
+        return Text(AppLocalizations.of(context)!.product_loading);
+      },
     );
   }
 }
--- lib/show_product/product_page.dart
+++ lib/show_product/product_page.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:leboncoin/firebase/product_repository.dart';
-import 'package:leboncoin/product/models/product.dart';
 import 'package:leboncoin/show_product/widgets/product_card.dart';
 import 'package:leboncoin/show_product/widgets/product_list.dart';
 
@@ -19,16 +17,22 @@ class _ProductPageState extends State<ProductPage> {
     return FutureBuilder<List<ProductCard>>(
       future: ProductRepository().getAllProducts(),
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(AppLocalizations.of(context)!.adverts),
-          ),
-          backgroundColor: Colors.greenAccent.withOpacity(0.8),
-          body: CustomScrollView(
-            primary: false,
-            slivers: <Widget>[ProductList(snapshot.data! as List<ProductCard>)],
-          ),
-        );
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(AppLocalizations.of(context)!.adverts),
+            ),
+            backgroundColor: Colors.greenAccent.withOpacity(0.8),
+            body: CustomScrollView(
+              primary: false,
+              slivers: <Widget>[
+                ProductList(snapshot.data! as List<ProductCard>)
+              ],
+            ),
+          );
+        }
+
+        return Text(AppLocalizations.of(context)!.product_loading);
       },
     );
   }
