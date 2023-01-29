e8f4f396bc58d4c6bbc06f92d256fd686ecbb887
qrodrigues <quentin.rodrigues1912@gmail.com>
2023-01-29 14:14:55 +0100

    fix: l10n dans details d'une annonce

--- lib/show_product/pages/display_product_page.dart
+++ lib/show_product/pages/display_product_page.dart
@@ -5,6 +5,7 @@ import 'package:leboncoin/product/models/product.dart';
 import 'package:leboncoin/show_product/widgets/customAppBar.dart';
 import 'package:leboncoin/themes/color.dart';
 import 'package:leboncoin/themes/textstyle.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DisplayProductPage extends StatelessWidget {
   const DisplayProductPage(this.product, {super.key});
@@ -19,12 +20,12 @@ class DisplayProductPage extends StatelessWidget {
           padding: EdgeInsets.all(12),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                Padding(padding: EdgeInsets.only(top: 10, bottom: 10), child: Text('Image', style: product_detail_section),),
+                Padding(padding: EdgeInsets.only(top: 10, bottom: 10), child: Text(AppLocalizations.of(context)!.detail_title_image, style: product_detail_section),),
                 Image.network(product.image1),
-                Padding(padding: EdgeInsets.only(top: 20), child: Text('Description', style: product_detail_section),),
+                Padding(padding: EdgeInsets.only(top: 20), child: Text(AppLocalizations.of(context)!.detail_title_description, style: product_detail_section),),
             Text(product.description,
                 style: product_detail_description, textAlign: TextAlign.center),
-                Padding(padding: EdgeInsets.only(top: 20, bottom: 10), child: Text('Informations', style: product_detail_section),),
+                Padding(padding: EdgeInsets.only(top: 20, bottom: 10), child: Text(AppLocalizations.of(context)!.detail_title_informations, style: product_detail_section),),
             Text('Date de publication : ' + DateFormat("dd/MM/yyyy à hh:mm").format(product.date.toDate()),
                 style: product_detail_description),
             Text('Prix : ' + product.price.toInt().toString() + '€',
