2bbe3c113f109b81b5768164fcdc4caa333f374c
qrodrigues <quentin.rodrigues1912@gmail.com>
2023-06-01 15:41:30 +0200

    fix: modification de l'ajout product pour utiliser bloc

---
+++ lib/add_product/Bloc/add_product_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'add_product_event.dart';
+part 'add_product_state.dart';
+
+class AddProductBloc extends Bloc<AddProductEvent, AddProductState> {
+  AddProductBloc() : super(AddProductInitial()) {
+    on<AddProductEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
